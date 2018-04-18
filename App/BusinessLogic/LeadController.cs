9ede55f4860c0cbd4ce03b69a10792e60bab10b9
1065490 <1065490@ucn.dk>
2018-04-18 13:39:18 +0200

    Added controllers

---
+++ App/BusinessLogic/LeadController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccesLayer;
+using Core;
+
+namespace BusinessLogic
+{
+
+    public class LeadController : ICRUD<Lead>
+    {
+        DBLead dbLead = new DBLead();
+
+        public void Create(Lead entity)
+        {
+            dbLead.Create(entity);
+        }
+
+        public void Delete(int id)
+        {
+            dbLead.Delete(id);
+        }
+
+        public Lead Read(int id)
+        {
+            return dbLead.Read(id);
+        }
+
+        public void Update(Lead entity)
+        {
+            dbLead.Update(entity);
+        }
+    }
+}
---
+++ App/BusinessLogic/MeetingController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccesLayer;
+using Core;
+
+namespace BusinessLogic
+{
+    public class MeetingController : ICRUD<Meeting>
+    {
+        public void Create(Meeting entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Meeting Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Meeting entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
